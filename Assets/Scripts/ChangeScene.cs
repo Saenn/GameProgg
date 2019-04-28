618c656a5d2bd6b9abdfaefc8cc330120f81ab95
Wasin Sirichaiekawat <>
2019-04-28 18:00:17 +0700

    add loadscene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string levelName;
+    public void OnTriggerEnter(Collider player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    public void clickButton(string destination) {
+        SceneManager.LoadScene(destination);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
